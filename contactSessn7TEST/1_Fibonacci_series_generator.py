329bb73c31b8ee98ed06feeee57d3f9298550869
Yemi Ogunleye <yemmy.akinyemmy@gmail.com>
2024-08-28 12:16:06 +0100

    commit 3

--- contactSessn7TEST/1_Fibonacci_series_generator.py
+++ contactSessn7TEST/1_Fibonacci_series_generator.py
@@ -7,5 +7,15 @@ def fibSeqGen():
         n3 = n1 + n2
         yield n1
 a = fibSeqGen()
-for i in range(10):
-    print(next(a), end=" ")
+while True:
+    try:
+        x = int(input('Enter a range of Fib Sequence to Generate (1 - 100): '))
+        if 1 <= x <= 100:
+            for i in range(x):
+                print(next(a), end=" ")
+            break
+        else:
+            print("Your input is invalid. Please enter a number between 1 and 100.")
+    except ValueError:
+        print("Your input is invalid. Please enter a valid integer.")
+
